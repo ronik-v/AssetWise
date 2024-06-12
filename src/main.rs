87895909ce1aec8e3d37bf3f39fe50a95f9989d2
ronik_v <andronnikovvv@gmail.com>
2024-06-12 16:49:35 +0300

    update: new version [many ticker input + logs[file]

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,22 @@ pub mod oracle;
 
 use std::io::{self, Write};
 use std::string::String;
+use std::sync::{Arc, Mutex};
 use std::thread;
-use std::sync::Arc;
 use std::time::Duration;
 
 use chrono::Local;
 use figlet_rs::FIGfont;
 use crate::data::moex_parser::Ticker;
 
+use crate::trade_report::Logger;
 use crate::models::arima::Arima;
 use crate::models::sma::Sma;
 use crate::signals::arima_signals::trade_signal_arima;
 use crate::signals::sma_signals::trade_signal_sma;
 
-fn trade_robot(ticker: Arc<String>, data: Ticker) {
+
+fn trade_robot(ticker: Arc<String>, data: Ticker, logger: Arc<Mutex<Logger>>) {
     let price_data = data.close;
 
     // Use models for getting trade states
@@ -39,8 +41,10 @@ fn trade_robot(ticker: Arc<String>, data: Ticker) {
     let sma_state = trade_signal_sma(sma5_time_series, sma12_time_series);
 
     // Logging states
-    trade_report::log_state(&ticker, arima_state, "ARIMA strategy".to_string(), (20, 0));
-    trade_report::log_state(&ticker, sma_state, "SMA strategy".to_string(), (21, 0));
+    println!("====================================================================");
+    logger.lock().unwrap().log_state(&ticker, arima_state, "ARIMA strategy".to_string());
+    logger.lock().unwrap().log_state(&ticker, sma_state, "SMA strategy".to_string());
+    println!("====================================================================");
 }
 
 fn main() {
@@ -53,36 +57,48 @@ fn main() {
     println!("{}", author_text.unwrap());
     println!("{}", license_text.unwrap());
 
-    let mut ticker = String::new();
-    print!("Enter your ticker >> ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut ticker).unwrap();
-    let ticker = ticker.trim().to_string();
-    let ticker = Arc::new(ticker);
+    let logger = Arc::new(Mutex::new(Logger::new()));
 
-    // Today
-    let today = Local::now().date_naive();
+    let tickers = Arc::new(Mutex::new(Vec::new()));
+    let tickers_clone = Arc::clone(&tickers);
+    let logger_clone = Arc::clone(&logger);
 
-    // String formatting to template - "yyyy-mm-dd"
-    let date_start = today.format("%Y-%m-%d").to_string();
-    let date_end = today.format("%Y-%m-%d").to_string();
-    let interval = 1;
+    thread::spawn(move || {
+        loop {
+            // Ticker input
+            let mut ticker = String::new();
+            io::stdout().flush().unwrap();
+            io::stdin().read_line(&mut ticker).unwrap();
+            let ticker = ticker.trim().to_string();
+            let ticker = Arc::new(ticker);
 
-    loop {
-        let ticker_data = data::moex_parser::get_ticker_data(ticker.clone(), date_start.clone(), date_end.clone(), interval);
-        match ticker_data {
-            Ok(data) => {
-                let ticker_clone = ticker.clone();
-                let handle = thread::spawn(move || {
-                    trade_robot(ticker_clone, data);
-                });
-                if let Err(e) = handle.join() {
-                    eprintln!("Error in trade_robot thread: {:?}", e);
+            tickers_clone.lock().unwrap().push(ticker.clone());
+
+            let logger = Arc::clone(&logger_clone);
+
+            thread::spawn(move || {
+                // Today
+                let today = Local::now().date_naive();
+                // String formatting to template - "yyyy-mm-dd"
+                let date_start = today.format("%Y-%m-%d").to_string();
+                let date_end = today.format("%Y-%m-%d").to_string();
+                let interval = 1;
+
+                loop {
+                    let ticker_data = data::moex_parser::get_ticker_data(ticker.clone(), date_start.clone(), date_end.clone(), interval);
+                    match ticker_data {
+                        Ok(data) => {
+                            trade_robot(ticker.clone(), data, Arc::clone(&logger));
+                        },
+                        Err(_) => {},
+                    }
+                    // Sleep
+                    thread::sleep(Duration::from_secs(60));
                 }
-            },
-            Err(e) => eprintln!("Error fetching data: {}", e),
+            });
         }
-        // Sleep
-        thread::sleep(Duration::from_secs(60));
+    });
+    loop {
+        thread::sleep(Duration::from_secs(1));
     }
 }
--- src/trade_report.rs
+++ src/trade_report.rs
@@ -1,16 +1,39 @@
 use chrono::Local;
-use crate::utils::states::States;
-use std::io;
+use std::collections::HashMap;
+use std::fs::OpenOptions;
 use std::io::Write;
+use std::sync::{Arc, Mutex};
+use crate::utils::states::States;
 
-pub fn log_state(ticker: &str, state: States, strategy_name: String, cursor_pos: (u16, u16)) {
-    let date_time = Local::now();
-    // Move the cursor to the specified position
-    print!("\x1B[{};{}H", cursor_pos.0, cursor_pos.1);
-    // Clear the line
-    print!("\x1B[K");
-    // Print the log message
-    println!("[{}] - {:?}: - {:?} - {:?}", date_time.format("%Y-%m-%d %H:%M:%S"), ticker, strategy_name, state);
-    // Flush the stdout buffer
-    io::stdout().flush().unwrap();
+pub struct Logger {
+    logs: Arc<Mutex<HashMap<Arc<String>, Vec<String>>>>,
+}
+
+impl Logger {
+    pub fn new() -> Self {
+        Logger {
+            logs: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+
+    pub fn log_state(&self, ticker: &Arc<String>, state: States, strategy: String) {
+        let date_time = Local::now();
+        let log_file = "app_log.txt";
+        let mut app_log_file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(log_file)
+            .expect("Unable to open file");
+        // prepare log message
+        let mut logs = self.logs.lock().unwrap();
+        let log_entry = format!("[{}] Ticker: {}, State: {:?}, Strategy: {}", date_time.format("%Y-%m-%d %H:%M:%S"), ticker, state, strategy);
+        let ticker_logs = logs.entry(ticker.clone()).or_insert_with(Vec::new);
+        ticker_logs.push(log_entry.clone());
+
+        // Logging in console
+        println!("\n{}\n", log_entry);
+        // Logging in log file
+        let prepared_log = log_entry.clone() + "\n";
+        app_log_file.write_all(prepared_log.as_bytes()).expect("Unable to open file");
+    }
 }

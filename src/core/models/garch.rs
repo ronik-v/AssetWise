cf01be79c64b5b91a42e0ea97bf430d3678f1c37
ronik-v <andronnikovvv@gmail.com>
2024-10-25 19:43:05 +0300

    update: garch model [volatile func]

--- src/core/models/garch.rs
+++ src/core/models/garch.rs
@@ -28,21 +28,34 @@ impl GARCH {
         log_income.iter().map(|&inc| inc - mean_price).collect::<Vec<f64>>()
     }
 
-    pub fn volatile(&self) -> Vec<f64> {
+    fn volatile(&self) -> Vec<f64> {
+        // GARCH(1, 1) volatile
         let log_income: Vec<f64> = self.log_income();
         let model_errors: Vec<f64> = self.model_errors(&log_income);
 
         let mut volatile_data: Vec<f64> = Vec::new();
-        for i in 0..model_errors.len() {
-            if i > 0 {
-                let value: f64 = W + A * (model_errors[i] * model_errors[i]) + B * volatile_data[i - 1];
-                volatile_data.push(value);
-            } else {
-                let value: f64 = W + A * (model_errors[i] * model_errors[i]);
-                volatile_data.push(value);
-            }
+        let value: f64 = W + A * (model_errors[0] * model_errors[0]);
+        volatile_data.push(value);
+
+        for i in 1..model_errors.len() {
+            let value: f64 = W + A * (model_errors[i] * model_errors[i]) + B * volatile_data[i - 1];
+            volatile_data.push(value);
         }
 
         volatile_data
+    }
+
+    pub fn price_volatile(&self) -> (Vec<f64>, Vec<f64>) {
+        // Volatile lines for price line
+        let volatile: Vec<f64> = self.volatile();
+        let mut volatile_up: Vec<f64> = Vec::new();
+        let mut volatile_down: Vec<f64> = Vec::new();
+
+        for i in 0..volatile.len() {
+            volatile_up.push(self.price_data[i] + volatile[i]);
+            volatile_down.push(self.price_data[i] - volatile_down[i]);
+        }
+
+        (volatile_up, volatile_down)
     }
 }
